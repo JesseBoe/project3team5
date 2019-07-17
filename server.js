cfd74f933f9a208aa2da279880e87902d17a7e4e
JesseBoe <JesseBoe@gmail.com>
2019-07-17 13:08:29 -0700

    heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 const path = require("path");
