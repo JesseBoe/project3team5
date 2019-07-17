26c5ccb0ef6c2150a6586adefa953f464aa467d6
JesseBoe <JesseBoe@gmail.com>
2019-07-17 12:42:53 -0700

    heroku

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //SocketIO
-const http = require('http');
+
 const io = require('socket.io')();
-const server = http.createServer();
+io.attach(app);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   console.log("PRODUCTION ALERT");
   app.use(express.static("client/build"));
 }
+
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected.`);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected.`);
+  });
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -23,17 +31,3 @@ app.get("*", function(req, res) {
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
-//SocketIO
-io.attach(server);
-
-io.on('connection', (socket) => {
-  console.log(`Socket ${socket.id} connected.`);
-
-  socket.on('disconnect', () => {
-    console.log(`Socket ${socket.id} disconnected.`);
-  });
-});
-
-server.listen(PORT);
-console.log("Socket Server is now listening on port: " + PORT);
