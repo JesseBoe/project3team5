e6ebad6fb9fd9a7df8d8340cd1a2297446366428
Kate <you@example.com>
2019-07-23 18:08:34 -0700

    Katies changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SocketConnecter from "./components/SocketConnecter";
+import NavBar from "./components/NavBar/index";
 
 class App extends Component {
   render() {
     return (
-      <SocketConnecter/>
-    );
+      // <SocketConnecter/>
+      <NavBar/>   
+       );
   }
 }
 
