a875de378f74f2992f0d5b7decbfb3101cd6c874
Wendy Wigg <wiggwendy@gmail.com>
2019-07-25 21:22:58 -0700

    login should work on heroku now

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import SocketConnector from "./components/SocketConnecter";
 import Login from "./components/Login";
 import HangmanGame from "./components/HangmanGame";
 
-const localhostSocket = io.connect("http://localhost:3001");
-//console.log(localhostSocket);
+//const localhostSocket = io.connect("http://localhost:3000");
+const PORT = process.env.PORT || 3001;
+console.log("http://" + window.location.hostname + ":" + PORT);
+const localhostSocket = io.connect("http://" + window.location.hostname + ":" + PORT);
 
 
 class App extends Component {
