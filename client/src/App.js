8a782dac871e16b1bfe1de92cc62272f82ab8c31
JesseBoe <JesseBoe@gmail.com>
2019-07-25 23:40:47 -0700

    Organizing

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import openSocket from "socket.io-client"
 
-import SocketConnecter from "./components/SocketConnecter";
-import VirtualKeyboard from "./components/VirtualKeyboard";
-import Timer from "./components/Timer";
-import Chat from "./components/Chat";
-
+import Chat from "./components/Chat/Chat.js";
 
 class App extends Component {
   socket = openSocket(window.location.host);
