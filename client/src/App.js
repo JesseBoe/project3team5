9178549750f61788c329f7188fc0f114c599cc43
Craig Simmons <craig.a.simmons@gmail.com>
2019-07-28 16:01:35 -0700

    Svaing - wheel elements showing up in canvas

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import io from "socket.io-client";
 import SocketConnector from "./components/SocketConnecter";
-import Login from "./components/Login";
 import HangmanGame from "./components/HangmanGame";
+import Login from "./components/Login";
+import Wheel from "./components/Wheel";
+import Winwheel from './components/Winwheel';
 
 //const localhostSocket = io.connect("http://localhost:3000");
 const PORT = process.env.PORT || 3001;
 console.log("http://" + window.location.hostname + ":" + PORT);
 const localhostSocket = io.connect("http://" + window.location.hostname + ":" + PORT);
-
+console.log(Winwheel + " app.js - Client")
 
 class App extends Component {
  
@@ -16,6 +18,7 @@ class App extends Component {
     return (
       <Login socket={localhostSocket}>
         <SocketConnector />
+        <Wheel />
         <HangmanGame />
       </Login>
     );
---
+++ client/src/components/Wheel.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import Winwheel from './Winwheel';
+
+class Wheel extends Component {
+    constructor() {
+        super();
+        let theWheel = new Winwheel( {
+            'canvasId'    : 'canvas',
+            'lineWidth'   : 3,
+            'numSegments' : 18,
+            'strokeStyle' : '#fcfbfe', 
+            'direction'    : 'clockwise',
+            'repeat'       : 0,
+            'textAlignment'  : 'center',
+            'textOrientation' : 'vertical',    // Set orientation. horizontal, vertical, curved.
+            'textFontFamily'  : 'verdana',     // Monospace font best for vertical and curved.
+            'textFontSize'    : 24,
+            'outerRadius'  : 220, 
+            'centerX'     : 250,    
+            'centerY'     : 255,
+            'rotationAngle'   : 0, 
+            'innerRadius'   : 75,  
+            'animation' :
+            {
+                'type'     : 'spinToStop',
+                'duration' : 3,
+                'spins'    : 9,
+                'callbackFinished' : 'spinResult()'
+            },
+            'segments'    :
+            [   
+                {'fillStyle' : '#000000', 'textFontSize' : 14, 'textFillStyle' : '#ffffff', 'text' : '  BANKRUPT'},
+                {'fillStyle' : '#7649fe', 'text' : '500'},
+                {'fillStyle' : '#0c164f', 'textFillStyle' : '#ffffff', 'text' : '800'}, 
+                {'fillStyle' : '#5643fd', 'text' : '600'},
+                {'fillStyle' : '#ba1e68', 'text' : '300'},
+                {'fillStyle' : '#1d1135', 'textFillStyle' : '#ffffff', 'text' : '400'},
+                {'fillStyle' : '#ffffff', 'textFontSize' : 14, 'text' : ' LOSE TURN'},
+                {'fillStyle' : '#7649fe', 'text' : '900'},
+                {'fillStyle' : '#0c164f', 'textFillStyle' : '#ffffff', 'text' : '700'}, 
+                {'fillStyle' : '#5643fd', 'text' : '600'},
+                {'fillStyle' : '#ba1e68', 'text' : '800'},
+                {'fillStyle' : '#1d1135', 'textFillStyle' : '#ffffff', 'text' : '500'},
+                {'fillStyle' : '#000000', 'textFontSize' : 14, 'textFillStyle' : '#ffffff', 'text' : '  BANKRUPT'},
+                {'fillStyle' : '#7649fe', 'text' : '1000'}, 
+                {'fillStyle' : '#0c164f', 'textFillStyle' : '#ffffff', 'text' : '400'},
+                {'fillStyle' : '#5643fd', 'text' : '700'},
+                {'fillStyle' : '#ba1e68', 'text' : '900'},
+                {'fillStyle' : '#1d1135', 'textFillStyle' : '#ffffff', 'text' : '2500'}
+            ],
+            'pointerGuide' :  
+            {
+                'display'     : false,
+                'strokeStyle' : '#ff3300',
+                'lineWidth'   : 3
+            },
+            'pins' : 
+            {
+                'number'      : 36,
+                'outerRadius' : 3,
+                'margin'      : 5,
+                'fillStyle'   : 'black',
+                'strokeStyle' : 'white'
+            }
+        });
+
+        function spinWheel() {
+            theWheel.rotationAngle = (theWheel.rotationAngle % 360); 
+            theWheel.startAnimation();
+        }
+
+        function spinResult() {
+            let prizeSegment = theWheel.getIndicatedSegment();
+            if (prizeSegment.text === '  BANKRUPT') {
+                alert("OMG. You've gone Bankrupt!");
+            } else if (prizeSegment.text === 'BANKRUPT') {
+                alert("You've lost your turn ");
+            } else {
+                alert("You have won " + prizeSegment.text);       
+            }
+        }
+    }
+    render() {
+        return (
+            <div id="canvasContainer">
+                <canvas id="canvas" width="500" height="500">
+                    Canvas not supported, use another browser.
+                </canvas>
+                <img id="prizePointer" src="../images/basic_pointer.png" alt="V" />
+            </div>
+        );
+    }
+}
+
+export default Wheel;
