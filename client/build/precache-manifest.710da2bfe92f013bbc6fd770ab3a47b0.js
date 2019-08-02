5c4147fe5a5345aed125c9dd5a37ed810985ae8c
Wendy Wigg <wiggwendy@gmail.com>
2019-08-01 21:08:32 -0700

    login auth bugs fixed round 2

---
+++ client/build/precache-manifest.710da2bfe92f013bbc6fd770ab3a47b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60f7c4339666f175d560",
+    "url": "/static/css/main.6a9ea407.chunk.css"
+  },
+  {
+    "revision": "60f7c4339666f175d560",
+    "url": "/static/js/main.6acd1357.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85a5f3a7891c1819bb1a",
+    "url": "/static/js/2.bbd34777.chunk.js"
+  },
+  {
+    "revision": "6443eff8b33008d8e57e3c1bbdbc26c9",
+    "url": "/static/media/SayWhatLogo.6443eff8.svg"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "33c09198907e7fb5d0157f6414904ced",
+    "url": "/index.html"
+  }
+];
