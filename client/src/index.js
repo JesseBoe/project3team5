0e773c379c499ed4b58491d1587c63664a025dd9
Jesse Boe <41746188+JesseBoe@users.noreply.github.com>
2019-08-01 20:59:36 -0700

    Revert "Auth fixes"

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
