4141201937073763eb0d7c0869216d9d60ed13dd
JesseBoe <JesseBoe@gmail.com>
2019-08-04 14:35:15 -0700

    Sockets and Gamelogic: Part 1

--- client/src/components/PlayerAvatar/PlayerSection.js
+++ client/src/components/PlayerAvatar/PlayerSection.js
@@ -1,38 +1,27 @@
 import React, { Component } from "react";
 import NewAvatar from "./NewAvatar";
 
+
 class PlayerSection extends Component {
     render() {
         return(
-            <div className="d-flex justify-content-around">
-                <span style={{ position:"relative", display: "inline-block" }}>
-                    <div style={{ position: "absolute", color: "#ba1e68", top: "-34px", width:"140px", textAlign: "center", fontSize: "32px"}}>Jesse</div>
-                    <NewAvatar RobotAntenna="01" RobotColor="Red" RobotImage="01" />
-                    <div style={{position: "absolute", left: "7px", bottom: "4px", height: "24px", width: "90%", textAlign: "center", backgroundColor: "rgb(0, 0, 0, .75)", color: "white"}}>
-                        <span style={{opacity: "1", color: "lightgreen"}}>1,000</span>
-                    </div>
-                </span>
-                <span style={{ position: "relative", display: "inline-block" }}>
-                    <div style={{ position: "absolute", color: "#ba1e68", top: "-34px", width: "140px", textAlign: "center", fontSize: "32px" }}>Player 2</div>
-                    <NewAvatar RobotAntenna="03" RobotColor="Pink" RobotImage="03" />
-                    <div style={{ position: "absolute", left: "7px", bottom: "4px", height: "24px", width: "90%", textAlign: "center", backgroundColor: "rgb(0, 0, 0, .75)", color: "white" }}>
-                        <span style={{ opacity: "1", color: "lightgreen" }}>1,000</span>
-                    </div>
-                </span>
-                <span style={{ position: "relative", display: "inline-block" }}>
-                    <div style={{ position: "absolute", color: "#ba1e68", top: "-34px", width: "140px", textAlign: "center", fontSize: "32px" }}>Player 3</div>
-                    <NewAvatar RobotAntenna="02" RobotColor="Blue" RobotImage="02" />
-                    <div style={{ position: "absolute", left: "7px", bottom: "4px", height: "24px", width: "90%", textAlign: "center", backgroundColor: "rgb(0, 0, 0, .75)", color: "white" }}>
-                        <span style={{ opacity: "1", color: "lightgreen" }}>1,000</span>
-                    </div>
-                </span>
-                <span style={{ position: "relative", display: "inline-block" }}>
-                    <div style={{ position: "absolute", color: "#ba1e68", top: "-34px", width: "140px", textAlign: "center", fontSize: "32px" }}>Player 4</div>
-                    <NewAvatar RobotAntenna="04" RobotColor="Purple" RobotImage="04" />
-                    <div style={{ position: "absolute", left: "7px", bottom: "4px", height: "24px", width: "90%", textAlign: "center", backgroundColor: "rgb(0, 0, 0, .75)", color: "white" }}>
-                        <span style={{ opacity: "1", color: "lightgreen" }}>1,000</span>
-                    </div>
-                </span>
+            <div style={{ position: "absolute", left: 0, bottom: 0, width: '100%' }}>
+                <div className="container d-flex justify-content-around" style={{ position: "relative"}}>
+                    {this.props.gameData && this.props.gameData.players.map((player => {
+                        return (
+                            <span key={player.username}>
+                                <div style={{ position: "relative", display: "inline-block" }}>
+                                    <div style={{ position: "absolute", width: "140px", top: '-64px', textAlign: 'center', fontSize: "24px", color: player.ready ? "#1eba57" : "#ba1e68"}}>{player.ready ? "Ready" : "Not Ready"}</div>
+                                    <div style={{ position: "absolute", color: "#ba1e68", top: "-34px", width: "140px", textAlign: "center", fontSize: "32px" }}>{player.username}</div>
+                                    <NewAvatar RobotAntenna={player.robotAntenna} RobotColor={player.robotColor} RobotImage={player.robotBody} />
+                                    <div style={{ position: "absolute", left: "7px", bottom: "4px", height: "24px", width: "90%", textAlign: "center", backgroundColor: "rgb(0, 0, 0, .75)", color: "white" }}>
+                                        <span style={{ opacity: "1", color: "lightgreen" }}>{player.cash}</span>
+                                    </div>
+                                </div>
+                            </span>
+                        );
+                    }))}
+                </div>
             </div>
         )
     }
