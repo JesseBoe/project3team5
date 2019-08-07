965ddb4096b43eaa3a7236ddaac9404cb83c8921
Jesse Boe <41746188+JesseBoe@users.noreply.github.com>
2019-08-06 21:14:25 -0700

    Revert "test merge"

--- server.js
+++ server.js
@@ -71,7 +71,8 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   console.log("YOU ARE IN THE PRODUCTION ENV");
   app.use(
-    express.static(path.join(__dirname, "client/build/"))
+    "/static",
+    express.static(path.join(__dirname, "client/build/static"))
   );
   app.get(["/", "/login", "/signup", "/gameplay", "/profile", "/players"], (req, res) => {
     res.sendFile(path.join(__dirname, "client/build/"));
